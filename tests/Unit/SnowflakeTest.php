8e95546950927d8d2121f6e87d9af2993ceb86be
Yurun <admin@yurunsoft.com>
2021-07-12 16:19:58 +0800

    同步

--- tests/Unit/SnowflakeTest.php
+++ tests/Unit/SnowflakeTest.php
@@ -1,27 +1,28 @@
 <?php
+
 namespace Imi\Snowflake\Test;
 
-use PHPUnit\Framework\TestCase;
 use Imi\Snowflake\SnowflakeUtil;
+use PHPUnit\Framework\TestCase;
 
 class SnowflakeTest extends TestCase
 {
-    public function testBasic()
+    public function testBasic(): void
     {
         $this->assertTrue(!empty(SnowflakeUtil::id('testBasic')));
-        $this->assertTrue(strlen(SnowflakeUtil::id('testBasic')) <= 19);
+        $this->assertTrue(\strlen(SnowflakeUtil::id('testBasic')) <= 19);
     }
 
-    public function testWorkIDAndDataCenterId()
+    public function testWorkIDAndDataCenterId(): void
     {
         $this->assertTrue(!empty(SnowflakeUtil::id('test1')));
-        $this->assertTrue(strlen(SnowflakeUtil::id('test1')) <= 19);
+        $this->assertTrue(\strlen(SnowflakeUtil::id('test1')) <= 19);
 
         $this->assertTrue(!empty(SnowflakeUtil::id('test2')));
-        $this->assertTrue(strlen(SnowflakeUtil::id('test2')) <= 19);
+        $this->assertTrue(\strlen(SnowflakeUtil::id('test2')) <= 19);
 
         $this->assertTrue(!empty(SnowflakeUtil::id('test3')));
-        $this->assertTrue(strlen($id = SnowflakeUtil::id('test3')) <= 19);
+        $this->assertTrue(\strlen($id = SnowflakeUtil::id('test3')) <= 19);
 
         $this->assertEquals(1, SnowflakeUtil::parseId('test3', $id, true)['datacenter']);
         $this->assertEquals(2, SnowflakeUtil::parseId('test3', $id, true)['workerid']);
@@ -32,20 +33,21 @@ class SnowflakeTest extends TestCase
         $this->assertEquals(20, SnowflakeUtil::parseId('test4', $id, true)['workerid']);
     }
 
-    public function testBatch()
+    public function testBatch(): void
     {
         $datas = [];
 
-        for ($i = 0; $i < 10000; ++$i) {
+        for ($i = 0; $i < 10000; ++$i)
+        {
             $id = SnowflakeUtil::id('testBatch');
 
             $datas[$id] = 1;
         }
 
-        $this->assertEquals(10000, count($datas));
+        $this->assertCount(10000, $datas);
     }
 
-    public function testParseId()
+    public function testParseId(): void
     {
         $data = SnowflakeUtil::parseId('testParseId', '1537200202186752');
 
@@ -59,5 +61,4 @@ class SnowflakeTest extends TestCase
         $this->assertEquals(0, $data['datacenter']);
         $this->assertEquals(0, $data['sequence']);
     }
-
 }
